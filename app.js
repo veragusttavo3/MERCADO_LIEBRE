7c7cff983826a491b684de0845e5090def274c72
veragusttavo3 <veragusttavo3@gmail.com>
2024-01-31 18:46:49 -0300

    hide NAV in mobile version

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ const path = require("path");
 app.use(express.static("public"));
 // 5_Definir la escucha del servidor
 app.listen(8000,()=>console.log(`http://localhost:8000`))
-// 6_require y response con get. rutas de archivos
+// 6_require y response con get. rutas de archivos (home)
 app.get("/",(req,res)=>{res.sendFile(path.resolve(__dirname,"views/home.html")) })
+// 7_ruta home
+app.get("/home",(req,res)=>{res.sendFile(path.resolve(__dirname,"views/home.html"))})
+// 8_ruta login
+app.get("/register",(req,res)=>{res.sendFile(path.resolve(__dirname,"views/register.html"))})
+// 9_ruta register
+app.get("/login",(req,res)=>{res.sendFile(path.resolve(__dirname,"views/login.html"))})
+// 7_ruta home
+//app.get("/home",(req,res)=>{res.sendFile(path.resolve(__dirname,"views/home.html"))})
